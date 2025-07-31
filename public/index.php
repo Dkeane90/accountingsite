ed9b80d3ec1dd37b5ee8c564867dd5af18d2e19a
Dkeane90 <dkcreativestudios.jm@gmail.com>
2025-07-31 16:03:39 -0500

    Process section

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@
     <section>
         <!-- Navbar -->
         <nav>
-            <div class="flex flex-row justify-between items-center px-20 pt-8 mb-20">
+            <div class="flex flex-row justify-between items-center px-20 pt-8 mb-8">
                 <!-- logo -->
 
                 <a href="/" target="_blank">
@@ -40,24 +40,94 @@
     <section>
         <!-- Hero section -->
 
-        <div class="bg-[url(https://i.ibb.co/tpstzY53/hero.jpg)] bg-no-repeat bg-center bg-cover">
+        <div class="bg-[url(https://i.ibb.co/tpstzY53/hero.jpg)] bg-no-repeat bg-center bg-cover mb-40">
             <div class="flex flex-col items-center pt-40">
                 <div>
 
                     <img src="https://i.ibb.co/Xfg16zr7/accounting-consultants-white-small.png" alt="accounting-consultants-white-small" class="h-40">
                 </div>
-                <h1 class=" text-white text-5xl text-center font-black text-green-600 p-16 mb-8">ACCOUNTANTS AS <br>
+                <h1 class=" text-white text-6xl text-center font-black text-green-600 p-16 mb-8">ACCOUNTANTS AS <br>
                     INNOVATIVE AS YOU ARE
                 </h1>
-                <button class="mb-40 button-hero ">Let&#39s Chat</button>
+
+                <button type="button" href="google.com" target="" class="mb-20 button-hero ">Let&#39s Chat</button>
             </div>
 
 
         </div>
+    </section>
+    <!-- process section -->
+    <section>
+
+        <div class=" flex flex-col">
+            <!-- process -->
+            <div class="flex flex-col items-center justify-items-center mb-40">
+                <div class="h2-primary mb-8">
+                    <h2>OUR PROCESS</h2>
+                </div>
+                <img class="" src="https://i.ibb.co/tymnZVq/orange-divider.png" alt="orange-divider" class="w-30">
+
+            </div>
+        </div>
+        <!-- All processes -->
+        <div class="flex flex-row justify-between mb-40 ml-8 mr-8 ">
+            <!-- individual process Book-->
+            <div class=" flex flex-col items-center">
+                <!-- image -->
+                <div class="w-60 h-60 border border-orange-800 px-8 py-8 mb-8 flex flex-row items-center hover:scale-105 transition-transform duration-300">
+                    <img class="w-40 h-40 object-contain mx-auto" src="https://i.ibb.co/qYmRMgfq/open-book-icon-1.png" alt="open-book-icon-1">
+                </div>
+                <div class="flex flex-col items-center">
+
+                    <h2 class="h3-primary">Book</h2>
+                    <p class="paragraph-primary">Book A consultation.<br> Tell us about your business, and we meet to<br> review your specific needs.</p>
+                </div>
 
 
+            </div>
+            <!-- Individual process Engage-->
+            <div class=" flex flex-col items-center">
+                <!-- image -->
+                <div class="w-60 h-60 border border-orange-800 px-8 py-8 mb-8 flex flex-row items-center hover:scale-105 transition-transform duration-300">
+                    <img class="w-40 h-40 object-contain mx-auto" src="https://i.ibb.co/QtFsmSp/partner-handshake-icon-1.png" alt="partner-handshake-icon">
+                </div>
+                <div class="flex flex-col items-center">
+
+                    <h2 class="h3-primary">Engage</h2>
+                    <p class="paragraph-primary">Now we begin the process of setting up <br>or reviewing your existing books.</p>
+                </div>
 
 
+            </div>
+            <!-- Individual process Automate -->
+            <div class=" flex flex-col items-center">
+                <!-- image -->
+                <div class="w-60 h-60 border border-orange-800 px-8 py-8 mb-8 flex flex-row items-center hover:scale-105 transition-transform duration-300">
+                    <img class="w-40 h-40 object-contain mx-auto" src="https://i.ibb.co/g5GrmSq/setting-line-icon-1.png" alt="setting-line-icon-1">
+                </div>
+                <div class="flex flex-col items-center">
+
+                    <h2 class="h3-primary">Automate</h2>
+                    <p class="paragraph-primary">Book A consultation.<br> Tell us about your business, and we meet to<br> review your specific needs.</p>
+                </div>
+
+
+            </div>
+            <!-- Individual process control -->
+            <div class=" flex flex-col items-center">
+                <!-- image -->
+                <div class="w-60 h-60  border border-orange-800 px-8 py-8 mb-8 flex flex-row items-center hover:scale-105 transition-transform duration-300">
+                    <img class="w-40 h-40 object-contain mx-auto" src="https://i.ibb.co/cKhDwQzy/business-management-icon-1.png" alt="business-management-icon">
+                </div>
+                <div class="flex flex-col items-center">
+
+                    <h2 class="h3-primary">Control</h2>
+                    <p class="paragraph-primary">Accurate and complete books put you in control.</p>
+                </div>
+
+
+            </div>
+        </div>
     </section>
 
 
